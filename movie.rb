a5732887a88293fd72efac8cda5f6a292754f9ed
fly49 <thor.steinar9000@gmail.com>
2018-07-12 02:53:05 +0300

    Update movie.rb

--- movie.rb
+++ movie.rb
@@ -1,5 +1,6 @@
 class Movie
   attr_reader :link, :title, :year, :country, :date, :genre, :duration, :rating, :director, :cast
+  GENRES = %w( Adventure Comedy Drama Fantasy Crime Thriller Biography Action)
   
   def initialize(params)
     params.each do |key, value|
@@ -11,7 +12,22 @@ class Movie
     "#{@title} (#{@date}; #{@genre}) - #{@duration}"
   end
   
-  def has_genre?(str)
-    @genre.include?(str) ? true : raise("This is not a #{str.downcase} movie")
+  def month
+    Date::MONTHNAMES[@date.split('-')[1].to_i] || "Unknown"
   end
+  
+  def duration
+    @duration.chomp(" min").to_i
+  end 
+
+  def has_genre?(str)
+    if @genre.include?(str)
+      return true
+    elsif GENRES.include?(str)
+      return false
+    else
+      raise "Incorrect genre!"
+    end
+  end
+  
 end
