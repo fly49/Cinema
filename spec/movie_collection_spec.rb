3b7f4cb30d16b8098aec394c28ed72fc01c1b445
flymypie <flymaipie@gmail.com>
2018-07-19 16:04:36 +0000

    restruct

---
+++ spec/movie_collection_spec.rb
@@ -0,0 +1,20 @@
+require "movie_collection"
+
+describe MovieCollection do
+  
+  before do
+      @collection = MovieCollection.new('movies.txt')
+  end
+  
+  describe ".sort_by" do
+    
+    context "wrong param" do
+      it "returns error" do
+        expect(@collection.sort_by(:soundtrack)).to eql("Wrong param!")
+      end
+    end
+    
+  end
+  
+  
+end
