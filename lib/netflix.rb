2a51b6c9e3bb0531bd9dff5217c5ecc0fd00ce22
flymypie <flymaipie@gmail.com>
2018-08-02 19:39:34 +0000

    refactoring

--- lib/netflix.rb
+++ lib/netflix.rb
@@ -22,46 +22,38 @@ module Cinema
 
     def define_filter(name, from: nil, arg: nil, &block)
       if from && arg
-        raise('Filter doesn\'t exist!') unless filters[from]
+        raise('Filter doesn\'t exist!') unless filters.key?(from)
         filters.store(name,proc { |movie| filters[from].call(movie,arg) })
       else
         filters.store(name,block)
       end
     end
-    
-    
 
-    def show(hash = {})
-      movie =
-        if block_given?
-          @database.select { |m| yield m }
-        else
-          filter(hash)
-        end
-        .max_by { |m| m.rating * rand }
+    def show(**filters, &block)
+      movie = filter(filters).select(&block)
+                             .max_by { |m| m.rating * rand }
       withdraw_money(movie.title)
       super movie
     end
-  
+
     private
-    
-    def filter(hash)
-      hash.reduce(@database) do |f_data, (key, val)|
-        sel_block = filter_select_block(key,val)
-        f_data.select(&sel_block) if val
+
+    def filter(**filters)
+      filters.reduce(@database) do |f_data, (key, val)|
+        f_data.select { |movie| matches_filter?(movie, key, val) }
       end
     end
-    
-    def filter_select_block(key,val)
+
+    def matches_filter?(movie,key,val)
       if filters[key]
-        proc { |m| filters[key].call(m,val) }
+        filters[key].call(movie,val)
       elsif @database[0].respond_to?(key)
-        proc { |film| Array(film.send(key)).any? { |i| val === i } }
+        Array(movie.send(key)).any? { |i| val === i }
       else
         raise('Wrong param or filter name!')
       end
     end
-    
+
     def withdraw_money(movie_title)
       movie_price = how_much?(movie_title)
       raise('Not enough funds!') if @account < movie_price
