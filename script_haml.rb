f419db5acf0866d5a0646358f57070375eff1843
flymypie <flymaipie@gmail.com>
2018-08-15 17:18:07 +0000

    NetflixRenderer

--- script_haml.rb
+++ script_haml.rb
@@ -1,11 +1,29 @@
 require 'haml'
+require 'fileutils'
 require_relative 'lib/netflix'
 
-template = File.read('page.haml')
-haml_engine = Haml::Engine.new(template)
-output = haml_engine.render(Object.new, :netflix => Cinema::Netflix.new('movies.txt'))
-
-html_file = File.new('page.html')
-File.open(html_file, 'w') do |f|
-  f.write(output)
+module Cinema
+  class NetflixRenderer
+    attr_reader :template
+    
+    def initialize(haml_template)
+      @template = File.read(haml_template)
+    end
+    
+    def render_to(file_name,path=nil)
+      haml_engine = Haml::Engine.new(template)
+      output = haml_engine.render(Object.new,
+                                  :netflix => Netflix.new('movies.txt'))
+      unless path.nil?
+        FileUtils.mkdir_p(path) unless File.exist?(path)
+      else
+        path = File.dirname(__FILE__)
+      end
+      File.open(File.join(path, file_name), 'w') do |f|
+        f.write(output)
+      end
+    end
+  end
 end
+
+Cinema::NetflixRenderer.new('page.haml').render_to("page.html")
